a9b63fd28b9f4505a2315504e131fc9b510cf57a
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-04 22:20:55 +0530

    Added insertsort program

---
+++ InsertSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTechniques
+{
+    public class InsertSort
+    {
+        public void Display()
+        {
+            int[] arr = new int[5] { 8, 16, 24, 32, 40 };
+            int n = 5, i, j, val, flag;
+
+            Console.WriteLine("Insert sort ");
+            Console.WriteLine("Initial Array is");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            for (i=1;i<n;i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for(j=i-1;j>=0&&flag!=1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+            Console.WriteLine("After Aray is ");
+            for (i=0;i<n;i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}
