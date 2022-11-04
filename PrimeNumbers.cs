7f45c4d69f5632c4efd2f14ae090b68aa83fab00
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-04 22:27:54 +0530

    Added prime numbers program

---
+++ PrimeNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTechniques
+{
+    public class PrimeNumbers
+    {
+        public void Display()
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Number : ");
+            for (int i = 2; i < 1000; i++)
+            {
+                for(int j=2;j<=1000;j++)
+                {
+                    if (i!=j && i%j==0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if(isPrime)
+                {
+                    Console.WriteLine("\n" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
