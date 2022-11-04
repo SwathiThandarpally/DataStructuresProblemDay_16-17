b88f3122988dcd80b3282e2564ed533128b8a182
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-04 22:09:25 +0530

    Added program of data structure

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SortingTechniques
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSorting bubbleSorting = new BubbleSorting();
+            //bubbleSorting.Display();
+
+            //InsertSort insertSort = new InsertSort();
+            //insertSort.Display();
+
+            //Anagram anagram = new Anagram();
+            //anagram.Display();
+
+            //BinarySearch binary = new BinarySearch();
+            //binary.Display();
+
+            //MergeSort mergeSort = new MergeSort();
+            //mergeSort.Display();
+
+            //PermutationSort permutationSort = new PermutationSort();
+            //permutationSort.Display();
+
+            //PrimeNumbers primeNumbers = new PrimeNumbers();
+            //primeNumbers.Display();
+
+            ExtendPrimeNumber extendPrimeNumber = new ExtendPrimeNumber();
+            extendPrimeNumber.Display();
+
+            
+            Anagram anagram = new Anagram();
+            anagram.Display();
+        }
+    }
+}
